f84cd0a19585adba43fc6029973912699b7bcf84
NohaAboutaleb <nohamesllhy133@gmail.com>
2023-05-28 15:01:24 +0200

    retrieving posts by similarity

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -47,10 +48,16 @@ def post_detail(request, year, month, day, post):
     # Form for users to comment
     form = CommentForm()
 
+    #List of similar posts
+    post_tags_ids = post.tags.values_list('id',flat=True)
+    similar_posts = Post.published.filter(tags__in = post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags = Count('tags')).order_by('-same_tags','-publish')[:4]
+ 
+
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post, "comments": comments, "form": form},
+        {"post": post, "comments": comments, "form": form,"similar_posts":similar_posts},
     )
 
 
